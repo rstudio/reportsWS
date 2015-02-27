7dc53f273c67e1b8dc7b1274fadfaf9b025313e4
Garrett <grolemund@gmail.com>
2015-02-27 17:08:26 -0500

    adding nutrition data set

---
+++ R/nutrition.R
@@ -0,0 +1,9 @@
+#' Nutrition data
+#'
+#' Nutrition data on 8463 foods provided by the United States Department of Agriculture (USDA). The dataset contains the amount of 25 nutrients and macronutrients found in 100 grams of each food, as well as the name, total calories, and food group for each food.
+#' 
+#' The nutrition data was compiled from data sets in the \code{\link[usdanutrients]{usdanutrients}} package.
+#'  
+#' @source \url{http://www.ars.usda.gov/Services/docs.htm?docid=8964}
+#'
+"nutrition"
