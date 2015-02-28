2e447c0f543a6c1b3d13f78aca4eaf590d2897da
Garrett <grolemund@gmail.com>
2015-02-28 13:34:56 -0500

    Added first demo

--- R/util.R
+++ R/util.R
@@ -30,4 +30,17 @@ solution <- function(number = 1) {
   file.edit(newpath)
 }
 
+demo <- function(number = 1) {
+  if (!(number %in% 1:5)) stop("Unrecognized demo number")
+
+  filename <- switch(number,
+    "1" = "1-resume.Rmd")
+
+  path <- system.file("demos", filename, package = "reportsWS")
+  newpath <- file.path(getwd(), filename)
+  file.copy(path, newpath, copy.mode = FALSE)
+  file.edit(newpath)
+}
+
+
 
